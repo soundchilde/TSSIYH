70bdc1156de7a1b79b434457262bd605fab85e6b
soundchilde <125124430+soundchilde@users.noreply.github.com>
2023-02-25 12:40:42 +0530

    Add files via upload

---
+++ dbconnect.php
@@ -0,0 +1,4 @@
+<?
+	$db = @mysql_connect("localhost:3306", "soundci4_tssiyh","m3mb3r0ftss1yh") or die("Could not connect to MySQL"); //prev was soundch5_tssiyh
+	@mysql_select_db("soundci4_TSSIYH",$db) or die("Could not select database"); //soundch5_TSSIYH
+?>
